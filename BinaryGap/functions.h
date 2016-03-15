4640c49e4a9b4f6a730b6137504b81d97d67de86
MrPlatwnas <platonickola@hotmail.com>
2016-03-15 16:17:02 +0200

    Initial Commit

---
+++ BinaryGap/functions.h
@@ -0,0 +1,6 @@
+/*******************************************
+File: functions.h
+Author: Platon-Nikolaos Kiorpelidis
+Purpose: Binary Gap challenge functions header
+Date: 15 March 2016
+*******************************************/
---
+++ BinaryGap/main.c
@@ -0,0 +1,54 @@
+/*******************************************
+File: main.c
+Author: Platon-Nikolaos Kiorpelidis
+Purpose: Interface
+Date: 15 March 2016
+*******************************************/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+uint16_t solution(uint32_t input_number)
+{
+  bool binary_gap_edge = false;
+
+  uint16_t binary_gap_max = 0;
+  uint16_t binary_gap = 0;
+  uint8_t digit;
+  while(input_number > 0)
+  {
+    digit = input_number % 2;
+    input_number /= 2;
+
+    if(((!digit && binary_gap_max) || digit) && !binary_gap_edge)
+    {
+      if(!digit) binary_gap++;
+      binary_gap_edge = true;
+    }
+    else if(binary_gap_edge && !digit)
+    {
+      binary_gap++;
+    }
+    else if(binary_gap && binary_gap_edge && digit)
+    {
+      binary_gap_edge = false;
+      if(binary_gap > binary_gap_max) binary_gap_max = binary_gap;
+      binary_gap = 0;
+    }
+  }
+  return binary_gap_max;
+}
+
+int main(void)
+{
+  uint32_t input_number;
+
+  printf("Give a number: ");
+  scanf("%" SCNd32, &input_number);
+
+  uint16_t binary_gap;
+  binary_gap = solution(input_number);
+  printf("%d has %d binary gap\n", input_number, binary_gap);
+}
