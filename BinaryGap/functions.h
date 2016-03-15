f54c43ffa280737b488860654e12b97d5a022369
MrPlatwnas <platonickola@hotmail.com>
2016-03-15 16:20:11 +0200

    Removed executable and unnecessary files

--- BinaryGap/functions.h
+++
@@ -1,6 +0,0 @@
-/*******************************************
-File: functions.h
-Author: Platon-Nikolaos Kiorpelidis
-Purpose: Binary Gap challenge functions header
-Date: 15 March 2016
-*******************************************/
