c617fea51d0a17a7d12d5fb6bdfd3c9af580c69c
MrPlatwnas <platonickola@hotmail.com>
2016-03-16 15:11:52 +0200

    Optimized solution function & tidied up the code structure in main.c

--- CyclicRotation/main.c
+++ CyclicRotation/main.c
@@ -6,28 +6,27 @@ Date: 15 March 2016
 *******************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
-void solution(int16_t array[], uint8_t n_rows, uint8_t n_shifts)
+int16_t* solution(int16_t array[], uint8_t n_rows, uint8_t n_shifts)
 {
-  uint8_t index;
-  while(n_shifts)
-  {
-    uint16_t last_number = array[n_rows - 1];
-    for(index = n_rows - 1; index > 0; index--)
-    {
-      array[index] = array[index - 1];
-    }
-    array[0] = last_number;
-    n_shifts--;
-  }
+  int16_t *rotated_array = malloc(n_rows * sizeof(int16_t));
+  size_t array_index;
+  size_t rotated_array_index = n_shifts % n_rows;
+  for(array_index = 0; rotated_array_index != n_rows; ++array_index, ++rotated_array_index)
+    rotated_array[rotated_array_index] = array[array_index];
+  for(rotated_array_index = 0; array_index != n_rows; ++array_index, ++rotated_array_index)
+    rotated_array[rotated_array_index] = array[array_index];
+  return rotated_array;
 }
 
 int main(void)
 {
-  int16_t array[3] = {1, 2, 3};
-  solution(array, 3, 1);
-  printf("%d\n", array[0]);
-  printf("%d\n", array[1]);
-  printf("%d\n", array[2]);
+  int16_t *array = malloc(5 * sizeof(int16_t));
+  for(size_t i = 0; i < 5; i++) array[i] = i;
+
+  array = solution(array, 5, 5);
+
+  for(size_t i = 0; i < 5; i++) printf("%d ", array[i]);
 }
