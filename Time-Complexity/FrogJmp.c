920e57c2d6acb2632c6ccbbd1812232b86adea53
Platon-Nikolaos Kiorpelidis <platwnace@gmail.com>
2016-03-22 00:20:52 +0200

    Initial Commit

---
+++ Time-Complexity/FrogJmp.c
@@ -0,0 +1,38 @@
+/*******************************************
+File: FrogJmp.c
+Author: Platon-Nikolaos Kiorpelidis
+Purpose: Solution to FrogJmp problem
+Date: 21 March 2016
+Elapsed Time: 2 mins 47 secs
+*******************************************/
+
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t solution(uint32_t start_pos, uint32_t end_pos, uint32_t step)
+{
+  start_pos = end_pos - start_pos;
+  if(start_pos % step == 0) return (start_pos / step);
+  else return (start_pos / step) + 1;
+}
+
+int main(int argc, const char *argv[])
+{
+  uint32_t start_pos;
+  uint32_t end_pos;
+  uint32_t step;
+
+  printf("Input starting position: ");
+  scanf("%u", &start_pos);
+
+  printf("Input ending position: ");
+  scanf("%u", &end_pos);
+
+  printf("Input the step: ");
+  scanf("%d", &step);
+
+  uint32_t n_jumps = solution(start_pos, end_pos, step);
+  printf("Number of jumps is: %u\n", n_jumps);
+
+  return 0;
+}
